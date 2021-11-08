90e76277e32b5b8a7ef828d67c02abcc33e61053
charles yarney <charlesyarney@gmail.com>
2021-11-08 15:27:20 +0000

    Completed challenge with algorithim

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 //write your code here
+// Declare a function called averageInArray
+
+// intailize with one parameters called numbers
+
+// declare a variable called average intailized to 0
+
+// create a for loop to iterate over array to get every element in array
+
+// in the loop: add every element to the average variable
+
+// after the loop(outside the scoop of the for loop): divide average by the length of the array.
+
+// use array.length() of find how many elements are in the array
+
+function averageInArray(numbers){
+ let average = 0;
+ for(let i = 0; i < numbers.length; i++) {
+    average += numbers[i];
+  }
+  average = average / numbers.length;
+  return average
+}
+
+console.log(averageInArray([2,2,2,2,2,]))
